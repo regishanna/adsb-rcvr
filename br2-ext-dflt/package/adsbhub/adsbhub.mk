e8d71db78f99d7163dd00dd0b0a9630f7bfa341c
Regis HANNA <regis.hanna@gmail.com>
2024-12-04 11:51:06 +0100

    Ajout du package rust adsbhub, permettant de connecter dump1090 au reseau ADSBHub

---
+++ br2-ext-dflt/package/adsbhub/adsbhub.mk
@@ -0,0 +1,15 @@
+################################################################################
+#
+# adsbhub
+#
+################################################################################
+
+ADSBHUB_VERSION = d87161b2000cc36ba81a19923d898cb627fe90c9
+ADSBHUB_SITE = git@github.com:regishanna/adsbhub.git
+ADSBHUB_SITE_METHOD = git
+
+define ADSBHUB_INSTALL_INIT_SYSV
+	$(INSTALL) -m 755 -D $(BR2_EXTERNAL_DFLT_PATH)/package/adsbhub/S70adsbhub $(TARGET_DIR)/etc/init.d/S70adsbhub
+endef
+
+$(eval $(cargo-package))
